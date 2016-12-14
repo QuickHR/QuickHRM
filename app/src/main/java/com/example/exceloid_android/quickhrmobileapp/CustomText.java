05c9f540fdf3441665e922e7af7a7e408bc3badc
exceloid <chanduyaramala@gmail.com>
2016-12-14 13:31:36 +0530

    latestversion

--- app/src/main/java/com/example/exceloid_android/quickhrmobileapp/CustomText.java
+++ app/src/main/java/com/example/exceloid_android/quickhrmobileapp/CustomText.java
@@ -9,16 +9,16 @@ import android.widget.TextView;
 /**
  * Created by Exceloid-Android on 07-12-2016.
  */
-public class CustomText extends TextView {
+public class CustomText extends Activity {
     /*public CustomText(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
            // this.setTypeface(Typeface.createFromAsset(context.getAssets(),"fonts/Roboto-Italic.ttf"));
-    }*/
+    }
 
-   /* public CustomText(Context context, AttributeSet attributeSet){
+    public CustomText(Context context, AttributeSet attributeSet){
         super(context,attributeSet);
         this.setTypeface(Typeface.createFromAsset(context.getAssets(),"fonts/Roboto-Italic.ttf"));
-    }*/
+    }
 
     public CustomText(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -36,5 +36,5 @@ public class CustomText extends TextView {
         } else {
             super.setTypeface(Typeface.createFromAsset(getContext().getAssets(), "fonts/MaterialIcons-Regular.ttf"));
         }
-    }
+    }*/
 }
